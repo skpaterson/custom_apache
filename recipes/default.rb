ed2562697a88ea15a39b0acd02954312cb4bc99c
Stuart Paterson <spaterson@chef.io>
2018-03-28 15:01:22 +0100

    Foodcritic and Cookstyle passing, updated to use Chef v13.

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,9 +10,9 @@
 #################################################
 
 # Load from node attributes some variables we'll need.
-site_name = node.site.name
-content_owner = node.site.content.owner
-content_group = node.site.content.group
+site_name = node['site']['name']
+content_owner = node['site']['content']['owner']
+content_group = node['site']['content']['group']
 
 # Ensure the apt cache is up-to-date.
 apt_update 'Update the apt cache daily' do
@@ -26,13 +26,7 @@ end
 #################################################
 
 # Install additional Apache packages.
-packages = %w( libapache2-modsecurity libapache2-mod-spamhaus )
-
-packages.each do |p|
-  package 'install libapache2 package' do
-    package_name p
-  end
-end
+package %w( libapache2-modsecurity libapache2-mod-spamhaus )
 
 # Create the group that owns web content.
 group content_group do
@@ -49,10 +43,8 @@ user content_owner do
   group content_group
   home "/home/#{content_owner}"
   shell '/bin/bash'
-  supports({
-    manage_home: true,
-    non_unique: false
-  })
+  manage_home true
+  non_unique false
 end
 
 # Use the custom site resource to configure the website.
